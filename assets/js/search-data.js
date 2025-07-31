aa43c81730a57abd11814b2c79fc19b395c2957d
GiorgiArsenadze <54861034+GiorgiArsenadze@users.noreply.github.com>
2025-07-31 18:31:11 +0000

    Deploying to gh-pages from @ GiorgiArsenadze/GiorgiArsenadze.github.io@cacb98647a3e1c818a2cce4b03a6ea05c7ef661e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -423,8 +423,8 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
+          section: "News",},{id: "projects-shaping-dark-photon-spectral-distortions",
+          title: 'Shaping dark photon spectral distortions',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
