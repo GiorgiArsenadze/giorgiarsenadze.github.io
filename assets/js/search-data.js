73ea9afee0e0b09e043bb9044ae9c6af5e42a8a1
GiorgiArsenadze <54861034+GiorgiArsenadze@users.noreply.github.com>
2025-08-06 13:33:13 +0000

    Deploying to gh-pages from @ GiorgiArsenadze/GiorgiArsenadze.github.io@f43c28837f0034688736ff91b29358a82377b753 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -438,21 +438,11 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-coming-soon",
+          title: 'Coming soon...',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
             },},{id: "projects-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
